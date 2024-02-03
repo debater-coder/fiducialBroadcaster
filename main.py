13ac38f8c1f7d24cfafcb8580b360be9d726b35b
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-02-03 23:24:13 +1100

    feat: implement code estimation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pickle
+
 import cv2 as cv
 import numpy as np
 
@@ -7,14 +9,24 @@ detector_params = cv.aruco.DetectorParameters()
 dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250)
 aruco_detector = cv.aruco.ArucoDetector(dictionary, detector_params)
 
+with open("calibration.pkl", "rb") as f:
+    camera_matrix, dist = pickle.load(f)
+
 while input_video.grab():
     ret, image = input_video.read()
 
     corners, ids, rejected = aruco_detector.detectMarkers(image)
 
     if ids is not None:
-        image_detected = cv.aruco.drawDetectedMarkers(image, corners, ids)
-        cv.imshow("detected markers", image_detected)
+        ret, rvec, tvec = cv.solvePnP(np.array([
+            (-1, 1, 0), (1, 1, 0), (1, -1, 0), (-1, -1, 0)
+        ], dtype="double"), corners[0], camera_matrix, dist)
+
+        image = cv.aruco.drawDetectedMarkers(image, corners, ids)
+
+        image = cv.drawFrameAxes(image, camera_matrix, dist, rvec, tvec, 1)
+
+        cv.imshow("detected markers", image)
 
     if cv.waitKey(1) == ord('q'):
         break
